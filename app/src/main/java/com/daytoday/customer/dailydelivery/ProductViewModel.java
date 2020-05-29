6a0fec0c27313e9c6dbd8283de9aa7e10c17944b
Shuvam Kumar <shuvam1982000@gmail.com>
2020-05-29 19:20:04 +0530

    addition of product

---
+++ app/src/main/java/com/daytoday/customer/dailydelivery/ProductViewModel.java
@@ -0,0 +1,22 @@
+package com.daytoday.customer.dailydelivery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class ProductViewModel extends ViewModel {
+    private ProductRepo productRepo;
+    private MutableLiveData<List<Product>> liveData = new MutableLiveData<>();
+
+    public ProductViewModel() {
+        this.productRepo = new ProductRepo();
+    }
+
+    public LiveData<List<Product>> getProduct()
+    {
+        liveData = productRepo.requestProduct();
+        return liveData;
+    }
+}
--- app/src/main/java/com/daytoday/customer/dailydelivery/SearchFragment.java
+++ app/src/main/java/com/daytoday/customer/dailydelivery/SearchFragment.java
@@ -2,63 +2,53 @@ package com.daytoday.customer.dailydelivery;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class SearchFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    FirebaseFirestore firestore;
+    RecyclerView recyclerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        firestore = FirebaseFirestore.getInstance();
+        recyclerView = view.findViewById(R.id.products);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        firestore.collection("Buss-Info").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                Iterator iterator = queryDocumentSnapshots.getDocuments().iterator();
+                List<String> list = new ArrayList<>();
+                while (iterator.hasNext())
+                {
+                    QueryDocumentSnapshot snapshots =(QueryDocumentSnapshot) iterator.next();
+                    Log.i("msg",snapshots.getId());
+                    list.add(snapshots.getId());
+                }
+                AddProductAdapter addProductAdapter = new AddProductAdapter(getContext(),list);
+                recyclerView.setAdapter(addProductAdapter);
+            }
+        });
+        return view;
     }
 }
