ad20c570ac93099634b73c4bf6b56bf4f77b0fee
divyancod <dysu135@gmail.com>
2020-06-02 20:01:45 +0530

    bottomnavigationbar

--- app/src/main/java/com/daytoday/customer/dailydelivery/HomeScreenActivity.java
+++ app/src/main/java/com/daytoday/customer/dailydelivery/HomeScreenActivity.java
@@ -2,87 +2,67 @@ package com.daytoday.customer.dailydelivery;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
+import android.widget.Toast;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeScreenActivity extends AppCompatActivity {
+public class HomeScreenActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-
-    private BottomAppBar bottomAppBar;
-    private FloatingActionButton floatingActionButton;
-
-
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
-
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
-
-        bottomAppBar = findViewById(R.id.bottomBar);
-        floatingActionButton = findViewById(R.id.fab);
-        setSupportActionBar(bottomAppBar);
-
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment,new SearchFragment()).commit();
-            }
-        });
-
-
-
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        if(savedInstanceState==null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment, new ProductFragment()).commit();
+        }
     }
-
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.bottom_menu, menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.myproducts:
+                getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment, new ProductFragment()).commit();
+                break;
+            case R.id.search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment, new SearchFragment()).commit();
+                break;
+            case R.id.notification:
+                getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment, new NotificationFragment()).commit();
+                break;
+            case R.id.myprofile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment, new UserFragment()).commit();
+                break;
+        }
         return true;
     }
 
-
-
+    //-------------------------inflating search icon on navigation bar-------------------------------------
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id==R.id.myproducts)
-        {
-            getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment,new ProductFragment()).commit();
-            //finish();
-            return true;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu_top, menu);
+        return true;
+    }
+    //------------------action on search icon on navigation bar-------------------------------------------
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.scan_business:
+                Toast.makeText(this, "To be Added soon", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        if (id==R.id.scan)
-        {
-            getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment,new ScanFragment()).commit();
-            return true;
-        }
-        if (id==R.id.notification)
-        {
-            getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment,new NotificationFragment()).commit();
-            return true;
-        }
-        if (id==R.id.myprofile)
-        {
-            getSupportFragmentManager().beginTransaction().replace(R.id.switchFragment,new UserFragment()).commit();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
 
