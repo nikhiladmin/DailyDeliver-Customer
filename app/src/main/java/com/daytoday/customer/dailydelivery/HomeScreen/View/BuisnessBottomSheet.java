2bd90e2ade05271230c57213580da2944bbfcf2c
Shuvam Kumar <shuvam1982000@gmail.com>
2020-09-27 15:11:49 +0530

    Business Addition through QRCode

---
+++ app/src/main/java/com/daytoday/customer/dailydelivery/HomeScreen/View/BuisnessBottomSheet.java
@@ -0,0 +1,68 @@
+package com.daytoday.customer.dailydelivery.HomeScreen.View;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.daytoday.customer.dailydelivery.HomeScreen.Model.Product;
+import com.daytoday.customer.dailydelivery.Network.Response.YesNoResponse;
+import com.daytoday.customer.dailydelivery.R;
+import com.daytoday.customer.dailydelivery.Utilities.AppConstants;
+import com.daytoday.customer.dailydelivery.Utilities.SaveOfflineManager;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BuisnessBottomSheet extends BottomSheetDialogFragment  {
+    RecyclerView recyclerView;
+    List<Product> productList;
+
+    public BuisnessBottomSheet(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.BottomSheetDialogTheme);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.business_bottom_sheet,container,false);
+
+        recyclerView = view.findViewById(R.id.product_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        BusinessAdapter adapter = new BusinessAdapter(getContext(),productList);
+        adapter.setListener((BusinessAdapter.ClickListener) getContext());
+        recyclerView.setAdapter(adapter);
+        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                BottomSheetDialog dialog = (BottomSheetDialog) getDialog();
+                FrameLayout bottomSheet = (FrameLayout) dialog.findViewById(R.id.design_bottom_sheet);
+                BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+        return view;
+    }
+}
