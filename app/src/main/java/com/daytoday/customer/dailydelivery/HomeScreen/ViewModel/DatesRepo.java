c3c6884094273c6404300e7975efb3406e25b6d2
Shuvam Kumar <shuvam1982000@gmail.com>
2021-01-03 12:02:59 +0530

    Firebase Architecture Change

--- app/src/main/java/com/daytoday/customer/dailydelivery/HomeScreen/ViewModel/DatesRepo.java
+++ app/src/main/java/com/daytoday/customer/dailydelivery/HomeScreen/ViewModel/DatesRepo.java
@@ -6,10 +6,13 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
 import com.daytoday.customer.dailydelivery.Dates;
+import com.daytoday.customer.dailydelivery.HomeScreen.Model.Transaction;
 import com.daytoday.customer.dailydelivery.Network.ApiInterface;
 import com.daytoday.customer.dailydelivery.Network.Client;
 import com.daytoday.customer.dailydelivery.Network.Response.DayWiseResponse;
 import com.daytoday.customer.dailydelivery.Utilities.AppConstants;
+import com.daytoday.customer.dailydelivery.Utilities.AppUtils;
+import com.daytoday.customer.dailydelivery.Utilities.FirebaseUtils;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,101 +29,30 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class DatesRepo {
-    public MutableLiveData<List<Dates>> requestPendingList(String busscustId) {
-        MutableLiveData<List<Dates>> liveData = new MutableLiveData<>();
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        reference.child("Buss_Cust_DayWise").child(busscustId).child("Pending")
-                .addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        Iterator iterator = dataSnapshot.getChildren().iterator();
-                        List<Dates> list = new ArrayList<>();
-                        while (iterator.hasNext())
-                        {
-                            DataSnapshot currentSnapshot = (DataSnapshot)iterator.next();
-                            String year = currentSnapshot.child("Year").getValue().toString();
-                            String  mon = currentSnapshot.child("Mon").getValue().toString();
-                            String day = currentSnapshot.child("Day").getValue().toString();
-                            String quantity = currentSnapshot.child("quantity").getValue().toString();
-                            if (quantity != null)
-                            {
-                                Log.i("msg","true");
-                                list.add(new Dates(CalendarDay.from(Integer.parseInt(year),Integer.parseInt(mon),Integer.parseInt(day)),quantity));
-                            }
-                        }
-                        liveData.setValue(list);
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-        return liveData;
-    }
-
-    public MutableLiveData<List<Dates>> requestAcceptedList(String busscustId) {
-        rejectLiveDataApi = new MutableLiveData<>();
-        pendingLiveDataApi = new MutableLiveData<>();
-
-        MutableLiveData<List<Dates>> liveData = new MutableLiveData<>();
-        ApiInterface apiInterface = Client.getClient().create(ApiInterface.class);
-        Call<DayWiseResponse> dayWiseResponseCall = apiInterface.getDayWise(busscustId);
-        dayWiseResponseCall.enqueue(new Callback<DayWiseResponse>() {
+    public void requestTotalList(DatesViewModel datesViewModel, DatabaseReference reference) {
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onResponse(Call<DayWiseResponse> call, Response<DayWiseResponse> response) {
-                if (!response.body().getError()){
-                    for(int i=0;i<response.body().getRejected().size();i++)
-                    {
-                        response.body().getRejected().get(i).convertDates();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Transaction> transactionsList = new ArrayList<>();
+                datesViewModel.isLoading.setValue(false);
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+
+                while (iterator.hasNext()) {
+                    DataSnapshot snapshot = ((DataSnapshot)iterator.next());
+                    // This Numerical check is to avoid Monthly-Total Key
+                    if (AppUtils.isNumerical(snapshot.getKey())) {
+                        Transaction transaction = snapshot.getValue(Transaction.class);
+                        Log.i("message","transactions is " + transaction);
+                        transactionsList.add(transaction);
                     }
-                    rejectLiveDataApi.setValue(response.body().getRejected());
-                    List<Dates> dates = response.body().getAccepted();
-                    for(int i=0;i<dates.size();i++)
-                        response.body().getAccepted().get(i).convertDates();
-                    liveData.setValue(response.body().getAccepted());
+                    datesViewModel.getTotalLiveData().postValue(transactionsList);
                 }
             }
 
             @Override
-            public void onFailure(Call<DayWiseResponse> call, Throwable t) {
-                Log.i(AppConstants.ERROR_LOG,"Some Error Occurred in DateRepo Error is  : { " + t.getMessage() + " }");
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
-        return liveData;
-    }
-    public MutableLiveData<List<Dates>> rejectLiveDataApi;
-    public MutableLiveData<List<Dates>> pendingLiveDataApi;
-
-    public MutableLiveData<List<Dates>> requestCancelledList(String busscustId) {
-        MutableLiveData<List<Dates>> liveData = new MutableLiveData<>();
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        reference.child("Buss_Cust_DayWise").child(busscustId).child("Rejected")
-                .addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        Iterator iterator = dataSnapshot.getChildren().iterator();
-                        List<Dates> list = new ArrayList<>();
-                        while (iterator.hasNext())
-                        {
-                            DataSnapshot currentSnapshot = (DataSnapshot)iterator.next();
-                            String year = currentSnapshot.child("Year").getValue().toString();
-                            String  mon = currentSnapshot.child("Mon").getValue().toString();
-                            String day = currentSnapshot.child("Day").getValue().toString();
-                            String quantity = currentSnapshot.child("quantity").getValue().toString();
-                            if (quantity != null)
-                            {
-                                list.add(new Dates(CalendarDay.from(Integer.parseInt(year),Integer.parseInt(mon),Integer.parseInt(day)),quantity));
-                            }
-                        }
-                        liveData.setValue(list);
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-        return liveData;
     }
 }
--- app/src/main/java/com/daytoday/customer/dailydelivery/HomeScreen/ViewModel/DatesViewModel.java
+++ app/src/main/java/com/daytoday/customer/dailydelivery/HomeScreen/ViewModel/DatesViewModel.java
@@ -1,50 +1,56 @@
 package com.daytoday.customer.dailydelivery.HomeScreen.ViewModel;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.daytoday.customer.dailydelivery.Dates;
+import com.daytoday.customer.dailydelivery.HomeScreen.Model.Transaction;
+import com.daytoday.customer.dailydelivery.Utilities.FirebaseUtils;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 
 import java.util.List;
 
 public class DatesViewModel extends ViewModel {
     private DatesRepo datesRepo;
-    private MutableLiveData<List<Dates>> pendinglivedata = new MutableLiveData<>();
-    private MutableLiveData<List<Dates>> acceptedlivedata = new MutableLiveData<>();
-    private MutableLiveData<List<Dates>> canceledlivedata = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    private MutableLiveData<List<Transaction>> totalLiveData = new MutableLiveData<>();
     String busscustId;
 
     public DatesViewModel(String busscustId) {
         this.busscustId = busscustId;
         datesRepo = new DatesRepo();
+        isLoading.setValue(false);
     }
 
-    public MutableLiveData<List<Dates>> getPendingList()
-    {
-        pendinglivedata = datesRepo.requestPendingList(busscustId);
-        return pendinglivedata;
+    public MutableLiveData<List<Transaction>> getTotalList(CalendarDay day) {
+        isLoading.setValue(true);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("Buss_Cust_DayWise").child(busscustId)
+                .child(FirebaseUtils.getAllMonthPath("" + day.getYear(),day.getMonth() + ""));
+        datesRepo.requestTotalList(this,reference);
+        return totalLiveData;
     }
 
-    public MutableLiveData<List<Dates>> getAcceptedList()
-    {
-        acceptedlivedata = datesRepo.requestAcceptedList(busscustId);
-        return acceptedlivedata;
+    public MutableLiveData<List<Transaction>> getTotalLiveData() {
+        return totalLiveData;
     }
 
-    public MutableLiveData<List<Dates>> getCancelledList()
-    {
-        canceledlivedata = datesRepo.requestCancelledList(busscustId);
-        return canceledlivedata;
-    }
-
-    public MutableLiveData<List<Dates>> rejectDataFromApi()
-    {
-        return datesRepo.rejectLiveDataApi;
-    }
-
-    public MutableLiveData<List<Dates>> pendingDataFromApi()
-    {
-        return datesRepo.pendingLiveDataApi;
+    /**
+     *
+     * @param selectedDay
+     * @return Null if no data is present
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Transaction getTransaction(CalendarDay selectedDay){
+        return totalLiveData.getValue().stream()
+                .filter(transaction -> transaction.getYear().equals("" + selectedDay.getYear())
+                        && transaction.getMon().equals("" + selectedDay.getMonth())
+                        && transaction.getDay().equals("" + selectedDay.getDay()))
+                .findFirst()
+                .orElse(null);
     }
 }
